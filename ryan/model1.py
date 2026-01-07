383fdca3a0f5d56052804f5a5f5795a42d2f2bbc
Ryan <ryanmichaud@uchicago.edu>
2026-01-06 18:59:57 -0600

    pls

--- ryan/model1.py
+++ ryan/model1.py
@@ -1,4 +1,23 @@
+"""
+from OptimizedDataGenerator4 import *
+import matplotlib.pyplot as plt
+import numpy as np
+import tensorflow as tf
+import keras_tuner as kt
+noGPU=False
+if noGPU:
+    tf.config.set_visible_devices([], 'GPU')
+
+print("\nHIIIIIIIIIIIIIIIIII\n")
+
+print(tf.config.experimental.list_physical_devices())
+print(tf.test.is_built_with_cuda())
+print(tf.test.is_built_with_gpu_support())
+print(tf.test.is_gpu_available())
+"""
+
 from abc import ABC, abstractmethod
+# import all the necessary libraries
 import tensorflow as tf
 from tensorflow.keras.layers import Input, Dense, Concatenate, Dropout
 from tensorflow.keras.models import Model
@@ -6,17 +25,24 @@ from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.optimizers.schedules import PolynomialDecay, ExponentialDecay, CosineDecay
 from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint
 import keras_tuner as kt
+from sklearn.metrics import roc_curve, auc
 from pathlib import Path
 import os
 import numpy as np
 import matplotlib.pyplot as plt
 import sys
-sys.path.append('/home/ryanmichaud/common_repo/Muon_Collider_Smart_Pixels/MuC_Smartpix_ML/')
-from Model_Classes import SmartPixModel
-from OptimizedDataGenerator4 import *
-from qkeras import QDense, QActivation, quantized_bits, quantized_relu
+sys.path.append("/local/d1/smartpixML/filtering_models/shuffling_data/") #TODO use the ODG from here
+import OptimizedDataGenerator4_data_shuffled_bigData as ODG2
+import pandas as pd
+from datetime import datetime
+sys.path.append("../ryan")
+import OptimizedDataGenerator4 as ODG
 
 
+sys.path.append(str(Path.cwd().parents[0]))
+
+from MuC_Smartpix_ML.Model_Classes import SmartPixModel
+print(SmartPixModel)
 
 class Model1(SmartPixModel):
     def __init__(self,
@@ -85,11 +111,11 @@ class Model1(SmartPixModel):
             # ── B) Architecture hyperparams ──────────────────────────────────────────
             # separately tune rows and cols
 
-            row1nodes      = hp.Int("1",   10, 200, step=10)
-            row2nodes      = hp.Int("2",   10, 200, step=10)
-            row3nodes      = hp.Int("3",   10, 200, step=10)
-            row4nodes      = hp.Int("4",   10, 200, step=10)
-            row5nodes      = hp.Int("5",   10, 200, step=10)
+            row1nodes      = hp.Int("1",   1, 30, step=1)
+            row2nodes      = hp.Int("2",   1, 30, step=1)
+            row3nodes      = hp.Int("3",   1, 30, step=1)
+            row4nodes      = hp.Int("4",   1, 30, step=1)
+            row5nodes      = hp.Int("5",   1, 30, step=1)
 
 
 
@@ -124,21 +150,85 @@ class Model1(SmartPixModel):
             return model
 
         tuner = kt.RandomSearch(
-        model_builder,
+        model_builder, 
         objective           = "val_binary_accuracy",
         max_trials          = 120,
         executions_per_trial = 2,
-        project_name        = "new_hyperparam_search"
+        project_name        = "hp_search_1_30"
         )
 
         tuner.search(
-            training_generator,
+            self.training_generator,
             validation_data = self.validation_generator,
             epochs          = 110,
             callbacks       = [
                 EarlyStopping(monitor='val_loss', patience=10, restore_best_weights=True)
             ]
         )
+
+
+    def makeUnquatizedModelHyperParameterTuning2(self):
+        def model_builder(hp):
+            # ── B) Architecture hyperparams ──────────────────────────────────────────
+            # separately tune rows and cols
+
+            row1nodes      = hp.Int("1",   1, 8, step=1)
+            row2nodes      = 16
+            row3nodes      = 15
+            row4nodes      = 26
+            row5nodes      = 12
+
+
+
+            input1 = tf.keras.layers.Input(shape=(1,), name="z_global")
+            input2 = tf.keras.layers.Input(shape=(1,), name="x_size")
+            input3 = tf.keras.layers.Input(shape=(1,), name="y_size")
+            input4 = tf.keras.layers.Input(shape=(1,), name="y_local")
+
+            ## concatenate the inputs into one layer
+            inputList = [input1, input2, input3, input4]
+            inputs = tf.keras.layers.Concatenate()(inputList)
+
+
+            ## here i will add the layers 
+
+            # layer 1
+            x = tf.keras.layers.Dense(row1nodes,activation='relu')(inputs)
+            x = tf.keras.layers.Dense(row2nodes, activation='relu')(x)
+            x = tf.keras.layers.Dense(row3nodes, activation='relu')(x)
+            x = tf.keras.layers.Dense(row4nodes, activation='relu')(x)
+            x = tf.keras.layers.Dense(row5nodes, activation='relu')(x)
+            output = tf.keras.layers.Dense(1,activation='sigmoid')(x)
+
+            model = tf.keras.Model(inputs=inputList, outputs=output)
+
+            model.compile(
+            optimizer="adam",
+            loss="binary_crossentropy",
+            metrics=["binary_accuracy"],
+            run_eagerly  = True 
+            )
+            return model
+
+        tuner = kt.RandomSearch(
+        model_builder, 
+        objective           = "val_binary_accuracy",
+        max_trials          = 120,
+        executions_per_trial = 2,
+        project_name        = "hp_search_1_8"
+        )
+
+        tuner.search(
+            self.training_generator,
+            validation_data = self.validation_generator,
+            epochs          = 110,
+            callbacks       = [
+                EarlyStopping(monitor='val_loss', patience=10, restore_best_weights=True)
+            ]
+        )
+
+
+        
      
 
 
@@ -149,6 +239,7 @@ class Model1(SmartPixModel):
         
             print(f"Building {config_name} model...")
             self.makeQuantizedModel_withBits(total_bits=total_bits,int_bits=int_bits)
+
     def makeQuantizedModel_withBits(self, total_bits = 8,int_bits =0):
         """
         Build & compile your QKeras model with the given number of integer bits.
@@ -252,90 +343,11 @@ class Model1(SmartPixModel):
     """
     
 
-    # """
-    # config_name = Unquantized or 
-    # config_name = f"quantized_{total_bits}w{int_bits}i"
-    # """
-    # def loadModel(self, file_path: str,config_name = "Unquantized"):
-    #     self.models[config_name]=tf.keras.models.load_model(file_path, compile=False)
+   
+def main():
+    m1 = Model1()
+    m1.loadTfRecords()  # must set self.training_generator and self.validation_generator
+    m1.makeUnquatizedModelHyperParameterTuning2()
 
-    # def saveModel(self, file_path = None,overwrite=False,config_name = "Unquantized"):
-    #     if file_path == None:
-    #         file_path = Path(f'./{self.modelName}.keras').resolve()
-    #     if not overwrite and os.path.exists(file_path):
-    #         raise Exception("Model exists. To overwrite existing saved model, set overwrite to True.")
-    #     self.models[config_name].save(file_path)
-    
-
-
-    #THESE SHOULD BE UNNECESSARY NOW, use the abstract version
-    def trainModel(self, epochs=100, batch_size=32, learning_rate=None, 
-                   save_best=True, early_stopping_patience=20,
-                   run_eagerly = False,config_name = "Unquantized"):
-        # raise NotImplementedError("Use the abstract class version, trainModel()")
-        print("\n\n\nWARNING SHOULD BE USING THE ABSTRACT VERSION INSTEAD\n\n\n")
-        print("\n\n\nTHIS IS FOR DEBUGGING WHY YOUR QUANTIZED MODEL IS RANDOMLY GUESSING\n\n\n")
-        print("\n\n\nONCE THAT IS FIGURED OUT, GO BACK TO THE ABSTRACT VERSION\n\n\n")
-        # self.loadTfRecords()
-        callbacks = []
-        print(self.models[config_name].summary())
-        self.models[config_name].compile(optimizer='adam', 
-                            loss='binary_crossentropy', 
-                            metrics=['binary_accuracy'],
-                            run_eagerly=True
-                            )
-        self.histories[config_name] = self.models[config_name].fit(x=self.training_generator,validation_data=self.validation_generator, callbacks=callbacks,epochs=epochs)
-
-
-    def trainUnquantizedModel(self): # in the input, specify the learning rate scheduler, etc.
-        raise NotImplementedError("Use the abstract class version, trainModel()")
-        self.loadTfRecords()
-        callbacks = []
-        self.models["Unquantized"].compile(optimizer='adam', 
-                            loss='binary_crossentropy', 
-                            metrics=['binary_accuracy'],
-                            run_eagerly=True
-                            )
-        self.histories["Unquantized"] = self.models["Unquantized"].fit(x=self.training_generator,validation_data=self.validation_generator, callbacks=callbacks,epochs=100)
-
-
-    def trainQuantizedModel(self): # in the input, specify the learning rate scheduler, etc.
-        raise NotImplementedError("Use the abstract class version, trainModel()")
-        self.loadTfRecords()
-        callbacks = []
-        # config_name = f"quantized_{total_bits}w{int_bits}i"
-        #TODO make this based off of total bits, int bits, in bit_configs
-        
-        self.models["quantized_8w0i"].compile(optimizer='adam', 
-                            loss='binary_crossentropy', 
-                            metrics=['binary_accuracy'],
-                            run_eagerly=True
-                            )
-        self.histories["quantized_8w0i"] = self.models["quantized_8w0i"].fit(x=self.training_generator,validation_data=self.validation_generator, callbacks=callbacks,epochs=100)
-
-    #plot based on history
-    def plotHistories(self):
-
-        def plotModelHistory(history,modelNum = -999):
-            plt.subplot(211)
-            # Plot training & validation loss values
-            plt.plot(history.history['loss'],label="Train")
-            plt.plot(history.history['val_loss'],label="Validation")
-            plt.title(f'Model {modelNum} loss and accuracy')
-            plt.ylabel('Loss')
-            # plt.xlabel('Epoch')
-            plt.legend()
-            plt.subplot(212)
-            # Plot training & validation accuracy values
-            plt.plot(history.history['binary_accuracy'],label="Train")
-            plt.plot(history.history['val_binary_accuracy'],label="Validation")
-            # plt.title(f'Model {modelNum} accuracy')
-            plt.ylabel('Accuracy')
-            plt.xlabel('Epoch')
-            plt.legend(loc='upper left')
-            plt.show()
-        if self.models["Unquantized"] is not None:
-            plotModelHistory(self.histories["Unquantized"], 1)
-        #TODO loop through bit_configs
-        if self.models["quantized_8w0i"] is not None:
-            plotModelHistory(self.histories["quantized_8w0i"], 2)
+if __name__ == "__main__":
+    main()
