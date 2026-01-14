1d82b3086f3172b9ebf56a5f8017c657ead0b1a2
Eliza Howard <elizachoward@gmail.com>
2026-01-13 23:35:30 -0600

    Added bib generation starting from writing track lists

--- MuC_Smartpix_Data_Production/Tracklist_Production/make_tracklists.py
+++ MuC_Smartpix_Data_Production/Tracklist_Production/make_tracklists.py
@@ -3,6 +3,9 @@ import pyLCIO
 import ROOT
 from math import *
 import argparse
+import os
+import random
+import matplotlib.pyplot as plt
 
 sensorAngles = np.arange(-np.pi,np.pi+2*np.pi/8,np.pi/8)
 
@@ -61,13 +64,44 @@ def getYlocalAndGamma(x,y):
 
 # user options
 parser = argparse.ArgumentParser(usage=__doc__, formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-parser.add_argument("-i", "--input_file", help="Input file", type=str, required=True)
-parser.add_argument("-o", "--output_file", help="Output file", type=str, required=True)
+parser.add_argument("-i", "--input_file", help="Input file", type=str)
+parser.add_argument("-odir", "--output_folder", help="Output folder", type=str)
 parser.add_argument("-f", "--float_precision", help="Floating point precision", default=5, type=int)
-parser.add_argument("-b", "--bin_size", help="Number of tracks per tracklist", default=1, type=int)
-parser.add_argument("-p", "--allowedPIDS", help="Allowed PIDs (comma-separated)", type=int, nargs="+", required=True)
+parser.add_argument("-t", "--track_total", help="Total number of tracks to simulate (for BIB and signal individually)", default=100, type=int)
+parser.add_argument("-b", "--bin_size", help="Number of tracks per tracklist", default=1, type=int) 
+parser.add_argument("-p", "--plot", help="Include if you want to make plots at this stage")
 parser.add_argument("-flp", "--flp", help="Direction of sensor (1 for FE side out, 0 for FE side down)", default=0, type=int)
+parser.add_argument("-sig", "--signal", help="Are you generating signal?", action='store_true')
+parser.add_argument("-bmm", "--bib_mm", help="Are you running mm bib?", action='store_true')
+parser.add_argument("-bmp", "--bib_mp", help="Are you running mp bib?", action='store_true')
+
+print("Here 1")
+
 ops = parser.parse_args()
+
+# get a list of files based on what you want to make tracklists for
+if ops.signal:
+    if not ops.input_file: 
+        raise Exception("You must specify an input file for signal")
+    if not ops.input_file.endswith(".slcio"):
+        raise Exception("Input file must be a .slcio file")
+    file_list=[ops.input_file]
+    output_file_form="signal_tracks_*.txt"
+elif ops.bib_mm or ops.bib_mp:
+    if ops.bib_mp:
+        directory_path = "/cvmfs/public-uc.osgstorage.org/ospool/uc-shared/public/futurecolliders/BIB10TeV/sim_mp_pruned/" 
+        output_file_form="bib_mp_tracks_*.txt"
+    else:
+        directory_path = "/cvmfs/public-uc.osgstorage.org/ospool/uc-shared/public/futurecolliders/BIB10TeV/sim_mm_pruned/" 
+        output_file_form="bib_mm_tracks_*.txt"
+    file_list=os.listdir(directory_path)
+    file_list = [os.path.join(directory_path, file) for file in file_list]
+else: 
+    raise Exception("You must include one of the flags for signal (-s), bib mm (-bmm), or bib mp (-bmp)")
+
+output_file=f"{ops.output_folder}/{output_file_form}"
+print("Here 2")
+print(output_file)
     
 # ############## SETUP #############################
 # Prevent ROOT from drawing while you're running -- good for slow remote servers
@@ -78,101 +112,172 @@ ROOT.gROOT.SetBatch()
 #    raise Exception(f"Directory {ops.output_file} does not exist")
 
 # convert this to writing to a log file
-print(f"Getting tracks from file: {ops.input_file}\n")
-print(f"Allowed PIDs: {ops.allowedPIDS}\n")
-
-
-if not ops.input_file.endswith(".slcio"):
-    raise Exception("Input file must be a .slcio file")
+#print(f"Getting tracks from file: {ops.input_file}\n")
+#print(f"Allowed PIDs: {ops.allowedPIDS}\n")
 
 tracks=[]
-reader = pyLCIO.IOIMPL.LCFactory.getInstance().createLCReader()
-reader.open(ops.input_file)
 
-for ievt,event in enumerate(reader):
-    print("Processing event %i."%ievt)
+track_count=0
+break_loop=False
+count = 0
+fileCountStart = 0
+fileCountLimit = 1000
+for file_path in file_list:
+    if break_loop:
+            break
+    count +=1
+    if count<fileCountStart:
+        continue
+    print("count", count)
+    if count>fileCountLimit:
+        break
+    # Get the full path to the file
+    if not file_path.endswith(".slcio"):
+        continue
+    print("Processing file: ", file_path)
+    reader = pyLCIO.IOIMPL.LCFactory.getInstance().createLCReader()
+    reader.open(file_path)
 
-    # Print all the collection names in the event
-    collection_names = event.getCollectionNames()
-
-    # Get vertex barrel hits
-    vtxBarrelHits = event.getCollection("VertexBarrelCollection")
-
-    for hit in vtxBarrelHits: 
-        position = hit.getPosition()
+    for ievt,event in enumerate(reader):
         
-        x,y,hit_z = position[0],position[1],position[2]
-        #rxy=(x**2+y**2)**0.5
-        t=hit.getTime()
+        print("Processing event %i."%ievt)
 
-        # Get layer
-        encoding = vtxBarrelHits.getParameters().getStringVal(pyLCIO.EVENT.LCIO.CellIDEncoding)
-        decoder = pyLCIO.UTIL.BitField64(encoding)
-        cellID = int(hit.getCellID0())
-        decoder.setValue(cellID)
-        #detector = decoder["system"].value()
-        layer = decoder['layer'].value()
-        #side = decoder["side"].value()
+        # Print all the collection names in the event
+        collection_names = event.getCollectionNames()
 
-        if layer!=0: continue # first layer only
+        # Get vertex barrel hits
+        vtxBarrelHits = event.getCollection("VertexBarrelCollection")
 
-        # get the particle that caused the hit
-        mcp = hit.getMCParticle()
-        hit_pdg = mcp.getPDG() if mcp else None
-        hit_id = mcp.id() if mcp else None
+        for hit in vtxBarrelHits: 
+            if track_count>ops.track_total-1:
+                print(f"\nReached total number of tracks ({ops.track_total}). Breaking loop.\n")
+                break_loop=True
+                break
 
-        if hit_id is None:
-            print("No MCParticle associated with hit, skipping.")
-            continue
+            position = hit.getPosition()
+            
+            x,y,hit_z = position[0],position[1],position[2]
+            #rxy=(x**2+y**2)**0.5
+            t=hit.getTime()
 
-        if abs(hit_pdg) not in ops.allowedPIDS: continue
+            # Get layer
+            encoding = vtxBarrelHits.getParameters().getStringVal(pyLCIO.EVENT.LCIO.CellIDEncoding)
+            decoder = pyLCIO.UTIL.BitField64(encoding)
+            cellID = int(hit.getCellID0())
+            decoder.setValue(cellID)
+            #detector = decoder["system"].value()
+            layer = decoder['layer'].value()
+            #side = decoder["side"].value()
 
-        # momentum at production
-        mcp_p = mcp.getMomentum()
-        mcp_tlv = ROOT.TLorentzVector()
-        mcp_tlv.SetPxPyPzE(mcp_p[0], mcp_p[1], mcp_p[2], mcp.getEnergy())
+            if layer!=0: continue # first layer only
 
-        # momentum at hit
-        hit_p = hit.getMomentum()
-        hit_tlv = ROOT.TLorentzVector()
-        hit_tlv.SetPxPyPzE( hit_p[0], hit_p[1], hit_p[2], mcp.getEnergy())
+            if ops.signal:
+                # get the particle that caused the hit
+                mcp = hit.getMCParticle()
+                hit_pdg = mcp.getPDG() if mcp else None
+                hit_id = mcp.id() if mcp else None
 
-        ylocal, gamma0 = getYlocalAndGamma(x,y)
-        zglobal = round(hit_z/25e-3)*25e-3 # round to nearest pixel
-        
-        # Define unit vector of track at tracker edge with respect to barrel
-        theta=hit_tlv.Theta()
-        phi=hit_tlv.Phi()
-        vx_global=np.sin(theta)*np.cos(phi)
-        vy_global=np.sin(theta)*np.sin(phi)
-        vz_global=np.cos(theta) 
+                if hit_id is None:
+                    print("No MCParticle associated with hit, skipping.")
+                    continue
 
-        # Convert vector to sensor frame to calculate alpha
-        vy_local=vx_global*np.sin(np.pi/2-gamma0)+vy_global*np.cos(np.pi/2-gamma0)
-        vx_local=vz_global
-        alpha=np.arctan2(vy_local,vx_local)
-        
-        # Calculate beta from phi and gamma0
-        beta=phi-(gamma0-np.pi/2)
+                if abs(hit_pdg) != 13: continue
 
-        # If we are unflipped, we must adjust alpha and beta, and flip y-local
-        if ops.flp == 0:
-            beta += np.pi
-            alpha = 2*np.pi-alpha
-            ylocal *= -1
+                # momentum at production
+                mcp_p = mcp.getMomentum()
+                mcp_tlv = ROOT.TLorentzVector()
+                mcp_tlv.SetPxPyPzE(mcp_p[0], mcp_p[1], mcp_p[2], mcp.getEnergy())
 
-        cota = 1./np.tan(alpha)
-        cotb = 1./np.tan(beta)
+                p = mcp_tlv.P()
+                pt = mcp_tlv.Pt()
 
-        p = mcp_tlv.P()
-        pt = mcp_tlv.Pt()
+                # momentum at hit
+                hit_p = hit.getMomentum()
+                hit_tlv = ROOT.TLorentzVector()
+                hit_tlv.SetPxPyPzE( hit_p[0], hit_p[1], hit_p[2], mcp.getEnergy())
+            else:
+                #momentum at hit
+                m_electron=.000511 #Gev
+                hit_p = hit.getMomentum() #Gev
+                hit_tlv = ROOT.TLorentzVector()
+                hit_tlv.SetPxPyPzE( hit_p[0], hit_p[1], hit_p[2], np.sqrt(m_electron**2+hit_tlv.P()**2))
 
-        # Skip tracks with momentum 0
-        if round(p, ops.float_precision)==0 or round(pt, ops.float_precision)==0:
-            continue
+                hit_pdg=11 #random.choice(11,-11)?
 
-        track = [cota, cotb, p, ops.flp, ylocal, zglobal, pt, t, hit_pdg]
-        tracks.append(track)
+                p = hit_tlv.P()
+                pt = hit_tlv.Pt()
+                if True:
+                    p1Calc = np.sqrt(hit_p[2]*hit_p[2]+hit_tlv.Pt()*hit_tlv.Pt())
+                    if np.abs(p1Calc/hit_tlv.P() -1)>0.000001:
+                        print(f"hit momentum {hit_p[2]}, pt {hit_tlv.Pt()}, p {hit_tlv.P()}, sqrt(pt^2 + pz^2 {p1Calc}")
+
+            if ops.plot:
+                # plt.plot1D("hit_mcp_e"  ,";mcp e [GeV];hits" , mcp_tlv.E(), 100, 0, 0.2)
+                # plt.plot1D("hit_mcp_pt"  ,";mcp pt [GeV];hits" , mcp_tlv.Pt(), 100, 0, 0.2)
+                # plt.plot1D("hit_mcp_eta" ,";mcp eta;hits" , mcp_tlv.Eta(), 100, -3.2, 3.2)
+                # plt.plot1D("hit_mcp_theta" ,";mcp theta;hits" , mcp_tlv.Theta(), 100, 0, 3.2)
+                # plt.plot1D("hit_mcp_phi" ,";mcp phi;hits" , mcp_tlv.Phi(), 100, -3.2, 3.2)
+                plt.plot1D("hit_pz"  ,";incident pz [GeV];hits" , hit_p[2], 100, 0, 0.2)
+                plt.plot1D("hit_pz_Long"  ,";incident pz [GeV];hits" , hit_p[2], 100, 0, 1)
+                plt.plot1D("hit_p"  ,";incident p [GeV];hits" , hit_tlv.P(), 100, 0, 0.2)
+                plt.plot1D("hit_p_Long"  ,";incident p [GeV];hits" , hit_tlv.P(), 100, 0, 1)
+                plt.plot1D("hit_pt"  ,";incident pt [GeV];hits" , hit_tlv.Pt(), 100, 0, 0.2)
+                plt.plot1D("hit_pt_Long"  ,";incident pt [GeV];hits" , hit_tlv.Pt(), 100, 0, 1)
+                plt.plot1D("hit_eta" ,";incident eta;hits" , hit_tlv.Eta(), 100, -3.2, 3.2)
+                plt.plot1D("hit_theta" ,";incident theta;hits" , hit_tlv.Theta(), 100, 0,3.2)
+                plt.plot1D("hit_phi" ,";incident phi;hits" , hit_tlv.Phi(), 100, -3.2, 3.2)
+                plt.plot1D("hit_eDep" ,";incident e deposit [MeV];hits" , hit.getEDep()*1000, 100, 0, 0.5)
+                # plt.plot1D("hit_mcp_prodrxy" ,";mcp prod rxy [mm];hits" , prodrxy, 100, 0,150)
+                # plt.plot1D("hit_mcp_prodz"   ,";mcp prod z [mm];hits" , prodz, 100, -1000,1000)
+                # plt.plot1D("hit_mcp_endrxy"  ,";mcp end rxy [mm];hits" , endrxy, 100, 0, 150)
+                # plt.plot1D("hit_mcp_endz"    ,";mcp end z [mm];hits" , endz, 100, -1000, 1000)
+
+                plt.plot1D("hit_time"    ,";hit time [ns];hits" , t, 100, -1, 5)
+                # print("phi particle,hit {:.2f} {:.2f}".format(hit_tlv.Phi(), mcp_tlv.Phi()))
+                # print("eta particle,hit {:.2f} {:.2f}".format(hit_tlv.Eta(), mcp_tlv.Eta()))
+
+                # double check if any bugs
+                phi = hit_tlv.Phi()
+                theta = hit_tlv.Theta()
+
+                plt.plot1D("hit_phi"    ,";cota;hits" , phi, 100, -10,10)
+                plt.plot1D("hit_theta"    ,";cotb;hits" , theta, 100, -10,10)
+                plt.plot1D("hit_t"    ,";t;hits" , t, 100, -1,10)
+
+            ylocal, gamma0 = getYlocalAndGamma(x,y)
+            zglobal = round(hit_z/25e-3)*25e-3 # round to nearest pixel
+            
+            # Define unit vector of track at tracker edge with respect to barrel
+            theta=hit_tlv.Theta()
+            phi=hit_tlv.Phi()
+            vx_global=np.sin(theta)*np.cos(phi)
+            vy_global=np.sin(theta)*np.sin(phi)
+            vz_global=np.cos(theta) 
+
+            # Convert vector to sensor frame to calculate alpha
+            vy_local=vx_global*np.sin(np.pi/2-gamma0)+vy_global*np.cos(np.pi/2-gamma0)
+            vx_local=vz_global
+            alpha=np.arctan2(vy_local,vx_local)
+            
+            # Calculate beta from phi and gamma0
+            beta=phi-(gamma0-np.pi/2)
+
+            # If we are unflipped, we must adjust alpha and beta, and flip y-local
+            if ops.flp == 0:
+                beta += np.pi
+                alpha = 2*np.pi-alpha
+                ylocal *= -1
+
+            cota = 1./np.tan(alpha)
+            cotb = 1./np.tan(beta)
+
+            # Skip tracks with momentum 0
+            if round(p, ops.float_precision)==0 or round(pt, ops.float_precision)==0:
+                continue
+
+            track = [cota, cotb, p, ops.flp, ylocal, zglobal, pt, t, hit_pdg]
+            tracks.append(track)
+            track_count+=1
 
 binsize = ops.bin_size
 float_precision = ops.float_precision
@@ -181,7 +286,7 @@ numFiles = int(np.ceil(len(tracks)/binsize))
 print(f"Writing {len(tracks)} tracks to {numFiles} files with {binsize} tracks per file\n")
 
 for fileNum in range(numFiles):
-    with open(ops.output_file.replace("*", str(fileNum)), 'w') as file:
+    with open(output_file.replace("*", str(fileNum)), 'w') as file:
         for track in tracks[fileNum*binsize:(fileNum+1)*binsize]:
 
             # set flp to an int
--- MuC_Smartpix_Data_Production/launchDataProduction.py
+++ MuC_Smartpix_Data_Production/launchDataProduction.py
@@ -52,54 +52,6 @@ def get_env_from_setup(script_path):
     return env
 
 def generate_signal():
-    # determine which step to start from
-    start_options = [ops.pgun_dir, ops.detsim_dir, ops.tracklist_dir, ops.pixelav_dir]
-    start_options_bool = [bool(x) for x in start_options]
-    if sum(start_options_bool) == 0:
-        start_step = 0  # start from particle gun
-
-    elif sum(start_options_bool) != 1:
-        raise ValueError("Please provide only one intermediate step directory to start from.")
-    else:
-        start_step = start_options_bool.index(True) + 1
-
-    # Use date and time to create unique output directory
-    if start_step==0:
-        output_dir = f"{repodir}/Data_Files/Data_Set_{datetime.now().strftime('%Y%m%d_%H%M%S')}" 
-        os.makedirs(output_dir)
-    else:
-        start_dir = start_options[start_step - 1]
-        output_dir = Path(start_dir).parent
-
-    # ADD CHECK TO MAKE SURE TOTAL TRACKS AND BIN SIZE ARE CONSISTENT WITH PROVIDED INTERMEDIATE FILES
-
-    # create output directories for each intermediate step
-    output_dir_pgun = f"{output_dir}/Particle_Gun"
-    output_dir_detsim = f"{output_dir}/Detector_Sim"
-    output_dir_tracklists = f"{output_dir}/Track_Lists"
-    output_dir_pixelav = f"{output_dir}/PixelAV"
-    output_dir_parquet = f"{output_dir}/Parquet_Files"
-
-    #output_dir_fluka = f"{output_dir}/FLUKA"
-
-    output_dirs=[output_dir_pgun, output_dir_detsim, output_dir_tracklists, output_dir_pixelav, output_dir_parquet]
-
-    if start_step==0:
-        for dir in output_dirs:
-            os.makedirs(dir)
-    else:
-        for dir in output_dirs[start_step:]:
-            shutil.rmtree(dir, ignore_errors=True) # make sure directory will be empty
-            os.makedirs(dir)
-
-    # set up MuColl environment
-    global env 
-    if start_step <= 3: 
-        # Load environment from relevant setup.sh for running a particle gun, detector simulation, and reading/processing resulting sclio file
-        env=os.environ.copy()
-        env.update(get_env_from_setup('/cvmfs/muoncollider.cern.ch/release/2.8-patch2/setup.sh'))
-    else:
-        env = None
 
     # Load evironment for parquet conversion process
     #env2 = os.environ.copy()
@@ -118,7 +70,6 @@ def generate_signal():
 
     signal_particle_gun = f"{output_dir_pgun}/particle_gun.slcio"
     signal_detetor_sim = f"{output_dir_detsim}/signal_detsim.slcio"
-    signal_tracklist = f"{output_dir_tracklists}/signal_tracks_*.txt"
 
     # Particle gun
     run_particle_gun = ["python3", f"{ops.benchmark_dir}/generation/pgun/pgun_lcio.py", 
@@ -139,11 +90,15 @@ def generate_signal():
     # Make tracklist
     make_tracklist = ["python3",  f"{repodir}/MuC_Smartpix_Data_Production/Tracklist_Production/make_tracklists.py", 
                         "-i", signal_detetor_sim, 
-                        "-o", signal_tracklist, 
+                        "-odir", output_dir_tracklists, 
                         "-f", str(ops.float_precision), 
                         "-b", str(ops.bin_size),
-                        "-p", "13", 
-                        "-flp", f"{ops.flp}"]
+                        "-flp", f"{ops.flp}", 
+                        "-t", str(ops.track_total),
+                        "-sig"]
+    
+    if ops.plot_in_maketracklists:
+        make_tracklist.append("-p")
 
     # Construct tuple of commands based on which step we are starting from
     if start_step == 0:
@@ -192,9 +147,9 @@ def generate_signal():
                         "-o", signal_parquet]
 
         # Construct tuple of commands based on which step we are starting from
-        if start_step <=3:
+        if start_step <= 3:
             command_tuple=(run_pixelAV, make_parquet,)
-        elif start_step ==4:
+        elif start_step == 4:
             command_tuple=(make_parquet,)
         commands.append([command_tuple,]) # weird formatting is because pool expects a tuple at input
 
@@ -202,23 +157,34 @@ def generate_signal():
     pool_commands(commands, ops.ncpu)
 
 def generate_bib():
-    if ops.tracklist_dir is None:
-        raise Exception("For now, you need to allready have tracks for bib, please specity directory")
-    
-    output_dir = Path(ops.tracklist_dir).parent
+    # Make tracklist
+    if start_step<=2:
+        make_tracklist_bibmm = ["python3",  f"{repodir}/MuC_Smartpix_Data_Production/Tracklist_Production/make_tracklists.py", 
+                            "-odir", output_dir_tracklists, 
+                            "-f", str(ops.float_precision), 
+                            "-b", str(ops.bin_size),
+                            "-flp", f"{ops.flp}", 
+                            "-t", str(int(ops.track_total/2)),
+                            "-bmm"]
+        
+        make_tracklist_bibmp = ["python3",  f"{repodir}/MuC_Smartpix_Data_Production/Tracklist_Production/make_tracklists.py", 
+                            "-odir", output_dir_tracklists, 
+                            "-f", str(ops.float_precision), 
+                            "-b", str(ops.bin_size),
+                            "-flp", f"{ops.flp}", 
+                            "-t", str(int(ops.track_total/2)),
+                            "-bmp"]
+        
+        if ops.plot_in_maketracklists:
+            make_tracklist_bibmm.append("-p")
+            make_tracklist_bibmp.append("-p")
+        
+        command_tuple=(make_tracklist_bibmp,make_tracklist_bibmm,)
 
-    # ADD CHECK TO MAKE SURE TOTAL TRACKS AND BIN SIZE ARE CONSISTENT WITH PROVIDED INTERMEDIATE FILES
+        commands= [[command_tuple,]] # weird formatting is because pool expects a tuple at input
 
-    # create output directories for each intermediate step
-    output_dir_tracklists = f"{output_dir}/Track_Lists"
-    output_dir_pixelav = f"{output_dir}/PixelAV"
-    output_dir_parquet = f"{output_dir}/Parquet_Files"
-
-    #output_dir_fluka = f"{output_dir}/FLUKA"
-
-    for dir in [output_dir_pixelav, output_dir_parquet]:
-        shutil.rmtree(dir, ignore_errors=True) # make sure directory will be empty
-        os.makedirs(dir)
+        # Run in parallel
+        pool_commands(commands, ops.ncpu)
 
     # 
     # Next run pixelAV and convert results to parquets
@@ -230,30 +196,36 @@ def generate_bib():
                     
     for run in range(nTracklists): 
 
-        # Define file names
-        bib_tracklist = f"{output_dir_tracklists}/bib_tracks_{run}.txt"
-        bib_pixelav_seed = f"{output_dir_pixelav}/bib_seed_{run}"
-        bib_pixelav_out = f"{output_dir_pixelav}/bib_pixelav_{run}.out"
-        bib_pixelav_log = f"{output_dir_pixelav}/bib_pixelav_log_{run}.txt"
-        bib_parquet = f"{output_dir_parquet}/bib_*_{run}.parquet" # include * here so it can be easily replaced when the different parquet files are written
-        
-        # Run pixelAV Muon_Collider_Smart_Pixels/MuC_Smartpix_Data_Production/PixelAV
-        run_pixelAV = [f"{repodir}/MuC_Smartpix_Data_Production/PixelAV", "./bin/ppixelav2_custom.exe", 
-                    "1", 
-                    bib_tracklist, 
-                    bib_pixelav_out, 
-                    bib_pixelav_seed,
-                    bib_pixelav_log
-                    ]
+        for bib_type in ["mm","mp"]:
 
-        # Write parquet file
-        make_parquet = ["python3", f"{repodir}/MuC_Smartpix_Data_Production/Data_Processing/datagen.py", 
-                        "-i", bib_pixelav_out, 
-                        "-o", bib_parquet]
+            # Define file names
+            bib_tracklist = f"{output_dir_tracklists}/bib_{bib_type}_tracks_{run}.txt"
+            bib_pixelav_seed = f"{output_dir_pixelav}/bib_{bib_type}_seed_{run}"
+            bib_pixelav_out = f"{output_dir_pixelav}/bib_{bib_type}_pixelav_{run}.out"
+            bib_pixelav_log = f"{output_dir_pixelav}/bib_{bib_type}_pixelav_log_{run}.txt"
+            bib_parquet = f"{output_dir_parquet}/bib_{bib_type}_*_{run}.parquet" # include * here so it can be easily replaced when the different parquet files are written
+            
+            # Run pixelAV Muon_Collider_Smart_Pixels/MuC_Smartpix_Data_Production/PixelAV
+            run_pixelAV = [f"{repodir}/MuC_Smartpix_Data_Production/PixelAV", "./bin/ppixelav2_custom.exe", 
+                        "1", 
+                        bib_tracklist, 
+                        bib_pixelav_out, 
+                        bib_pixelav_seed,
+                        bib_pixelav_log
+                        ]
 
-        
-        command_tuple=(run_pixelAV, make_parquet,)
-        commands.append([command_tuple,]) # weird formatting is because pool expects a tuple at input
+            # Write parquet file
+            make_parquet = ["python3", f"{repodir}/MuC_Smartpix_Data_Production/Data_Processing/datagen.py", 
+                            "-i", bib_pixelav_out, 
+                            "-o", bib_parquet]
+
+            
+            # Construct tuple of commands based on which step we are starting from
+            if start_step <= 3:
+                command_tuple=(run_pixelAV, make_parquet,)
+            elif start_step == 4:
+                command_tuple=(make_parquet,)
+            commands.append([command_tuple,]) # weird formatting is because pool expects a tuple at input
 
     # Run in parallel
     pool_commands(commands, ops.ncpu)
@@ -262,11 +234,13 @@ def generate_bib():
 parser = argparse.ArgumentParser(usage=__doc__, formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 parser.add_argument("-c", "--ncpu", help="Number of cores to use", default=35, type=int)
 parser.add_argument("-bs", "--bin_size", help="Number of tracks per track list", default=10, type=int)
-parser.add_argument("-t", "--track_total", help="Total number of tracks to simulate (for BIB and signal individually)", default=100, type=int)
+parser.add_argument("-t", "--track_total", help="Total number of tracks to simulate (for BIB and signal individually)", default=10, type=int)
 parser.add_argument("-f", "--float_precision", help="Floating point precision", default=5, type=int)
 parser.add_argument("-flp", "--flp", help="Direction of sensor (1 for FE side out, 0 for FE side down)", default=0, type=int)
 parser.add_argument("-bd", "--benchmark_dir", help="Muon collider simulation benchmark directory", default="/home/karri/mucLLPs/mucoll-benchmarks/", type=str)
-parser.add_argument("-sb", "--signal_or_bib", help="Generate just signal: 0, generate just bib: 1, generate both: 2", default=0, type=int)
+parser.add_argument("-p", "--plot_in_maketracklists", help="Make plots during the process of making tracklists", action='store_true')
+parser.add_argument("-sig", "--signal", help="Are you generating signal?", action='store_true')
+parser.add_argument("-bib", "--bib", help="Are you running bib?", action='store_true')
 
 # If you want to provide your own intermediate files as a starting point, specify their directories with these options
 parser.add_argument("-pg", "--pgun_dir", help="Directory containing particle gun output", default=None, type=str)
@@ -276,8 +250,8 @@ parser.add_argument("-pav", "--pixelav_dir", help="Directory containing pixelav 
 
 ops = parser.parse_args()
 
-if ops.signal_or_bib not in [0,1,2]:
-    raise ValueError("signal_or_bib (sb) argument must be 0, 1 or 2")
+if not ops.signal and not ops.bib:
+    raise ValueError("You must include -sig for signal and/or -bib for generating bib")
 
 # from bin size and total tracks, get number of track lists
 if ops.tracklist_dir is not None:
@@ -289,8 +263,56 @@ else:
 
 repodir = Path(__file__).resolve().parent.parent
 
-if ops.signal_or_bib in [0,2]:
+# determine which step to start from
+start_options = [ops.pgun_dir, ops.detsim_dir, ops.tracklist_dir, ops.pixelav_dir]
+start_options_bool = [bool(x) for x in start_options]
+if sum(start_options_bool) == 0:
+    start_step = 0  # start from particle gun
+
+elif sum(start_options_bool) != 1:
+    raise ValueError("Please provide only one intermediate step directory to start from.")
+else:
+    start_step = start_options_bool.index(True) + 1
+
+# Use date and time to create unique output directory
+if start_step==0:
+    output_dir = f"{repodir}/Data_Files/Data_Set_{datetime.now().strftime('%Y%m%d_%H%M%S')}" 
+    os.makedirs(output_dir)
+else:
+    start_dir = start_options[start_step - 1]
+    output_dir = Path(start_dir).parent
+
+# ADD CHECK TO MAKE SURE TOTAL TRACKS AND BIN SIZE ARE CONSISTENT WITH PROVIDED INTERMEDIATE FILES
+
+# create output directories for each intermediate step
+output_dir_pgun = f"{output_dir}/Particle_Gun"
+output_dir_detsim = f"{output_dir}/Detector_Sim"
+output_dir_tracklists = f"{output_dir}/Track_Lists"
+output_dir_pixelav = f"{output_dir}/PixelAV"
+output_dir_parquet = f"{output_dir}/Parquet_Files"
+
+#output_dir_fluka = f"{output_dir}/FLUKA"
+
+output_dirs=[output_dir_pgun, output_dir_detsim, output_dir_tracklists, output_dir_pixelav, output_dir_parquet]
+
+if start_step==0:
+    for dir in output_dirs:
+        os.makedirs(dir)
+else:
+    for dir in output_dirs[start_step:]:
+        shutil.rmtree(dir, ignore_errors=True) # make sure directory will be empty
+        os.makedirs(dir)
+
+# set up MuColl environment
+if start_step <= 3: 
+    # Load environment from relevant setup.sh for running a particle gun, detector simulation, and reading/processing resulting sclio file
+    env=os.environ.copy()
+    env.update(get_env_from_setup('/cvmfs/muoncollider.cern.ch/release/2.8-patch2/setup.sh'))
+else:
+    env = None
+
+if ops.signal:
     generate_signal()
 
-if ops.signal_or_bib in [1,2]:
+if ops.bib:
     generate_bib()
