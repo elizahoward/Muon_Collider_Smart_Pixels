Fixed momentum units, where pkls are saved.
Changed momentum units to [GeV/c] in all plots.
Changed that pkls are stored in the PLOT_DIR to be more sensible,
so that the pkls to regenerate plots are right in the directory
where the plots are, and so that when you rerun plots you don't
accidentally use pkls that don't exist.
	Tested appropriate behavior with old dataset and with
	Data_Set_20260119_151558

daniel/validationPlots/plotUtils.py
@@ -235,12 +235,20 @@ def processReconBibSig(truthDF,reconDF,doPrint=False):
     assert len(truthBib) == len(xSizesBib)
     assert len(truthBib) == len(ySizesBib)
     assert len(truthBib) == len(nPixelsBib)
-    truthSig.loc[:,"xSize"] = list(xSizesSig)
-    truthSig.loc[:,"ySize"] = list(ySizesSig)
-    truthSig.loc[:,"nPix"]  = list(nPixelsSig)
-    truthBib.loc[:,"xSize"] = list(xSizesBib)
-    truthBib.loc[:,"ySize"] = list(ySizesBib)
-    truthBib.loc[:,"nPix"]  = list(nPixelsBib)
+    # truthSig.loc[:,"xSize"] = list(xSizesSig)
+    # truthSig.loc[:,"ySize"] = list(ySizesSig)
+    # truthSig.loc[:,"nPix"]  = list(nPixelsSig)
+    # truthBib.loc[:,"xSize"] = list(xSizesBib)
+    # truthBib.loc[:,"ySize"] = list(ySizesBib)
+    # truthBib.loc[:,"nPix"]  = list(nPixelsBib)
+    #The error doesn't go away if use the top code
+    #And based on behavior, seems fine
+    truthSig["xSize"] = list(xSizesSig)
+    truthSig["ySize"] = list(ySizesSig)
+    truthSig["nPix"]  = list(nPixelsSig)
+    truthBib["xSize"] = list(xSizesBib)
+    truthBib["ySize"] = list(ySizesBib)
+    truthBib["nPix"]  = list(nPixelsBib)
 
     #from Eliza's code, the average yprofiles in each section
     print("getting average cluster profile in each regime")
@@ -446,7 +454,7 @@ def plotPt(truthSig,truthBib_mm,truthBib_mp,truthBib,PLOT_DIR='./plots',interact
     bins = np.linspace(-1,10,100)
     plotManyHisto([truthSig[key],truthBib_mm[key],truthBib_mp[key],truthBib[key]],title="",pltStandalone=False,
                   pltLabels=[f"sig {key}",f"bib mm {key}",f"bib mp {key}",f"bib {key}"],bins=bins,
-                  showNums=showNums,figsize=(7,2),yscale='log',xlabel="Momentum pT (GeV)",ylabel="Tracks (log scale)")
+                  showNums=showNums,figsize=(7,2),yscale='log',xlabel="Momentum pT [GeV/c]",ylabel="Tracks (log scale)")
     closePlot(PLOT_DIR, interactivePlots,  "bib_signal_pt_lowPtRange.png")
 
     plt.figure(figsize=(7, 5))
@@ -460,7 +468,7 @@ def plotPt(truthSig,truthBib_mm,truthBib_mp,truthBib,PLOT_DIR='./plots',interact
     # bins = np.logspace(-1,2.1,100)
     plotManyHisto([truthSig[key],truthBib_mm[key],truthBib_mp[key],truthBib[key]],title="",pltStandalone=False,
                   pltLabels=[f"sig {key}",f"bib mm {key}",f"bib mp {key}",f"bib {key}"],bins=bins,
-                  showNums=showNums,figsize=(7,2),yscale="log",xlabel="Momentum pT (GeV)",ylabel="Tracks (log scale)")
+                  showNums=showNums,figsize=(7,2),yscale="log",xlabel="Momentum pT [GeV/c]",ylabel="Tracks (log scale)")
     closePlot(PLOT_DIR, interactivePlots,  "bib_signal_pt_fullRange.png")
 
     if doPrint:
@@ -470,7 +478,7 @@ def plotPt(truthSig,truthBib_mm,truthBib_mp,truthBib,PLOT_DIR='./plots',interact
 def plotPtEta(truthSig,truthBib,PLOT_DIR='./plots',interactivePlots=False):
     plt.figure(figsize=(5,4))
     # plt.figure()
-    plotAvsB(truthSig,truthBib,"eta","pt",'η',"pT (GeV)","pT vs. η")
+    plotAvsB(truthSig,truthBib,"eta","pt",'η',"pT [GeV/c]","pT vs. η")
     closePlot(PLOT_DIR,interactivePlots,"signal_bib_ptEta.png")
 
 
@@ -667,6 +675,9 @@ def genEtaAlphaBetaRq(truthDF):
         truthDF['p_calc2'] = truthDF['pt']*np.sqrt(1+ 1/ (truthDF['cotAlpha']*truthDF['cotAlpha'] + truthDF['cotBeta']*truthDF['cotBeta']) )
         truthDF['p_calc3'] = truthDF['pt']*np.sqrt(1+ 1/ (truthDF['cotAlpha']*truthDF['cotAlpha'] + truthDF['cotBeta']*truthDF['cotBeta']) )
 
+        truthDF['betaGamma'] = truthDF['p_calc2'] / truthDF['m'] * 1000 #conversion from MeV to GeV for mass
+        #TODO: Decide which p calculation to use
+
     return truthDF
 
 # --- Plot 1: cotAlpha, cotBeta, number_eh_pairs, nPixels ---
@@ -723,7 +734,7 @@ def plotYlocalXYsize(truthbib, truthsig, xSizesSig, xSizesBib, ySizesSig, ySizes
 # --- Plot 4: 2D histogram of number_eh_pairs vs pt ---
 def plotEhPt(truthbib, truthsig, mask_bib,mask_sig,PLOT_DIR="./plots",interactivePlots=False):
     plot1by2BibSig2dHisto(truthbib,truthsig,'number_eh_pairs','pt',mask_bib,mask_sig,30,30,'Blues',
-                          'Number of electron hole pairs','pT (GeV)',"bib_signal_ehpairs_vs_pt_2d",PLOT_DIR, interactivePlots)
+                          'Number of electron hole pairs','pT [GeV/c]',"bib_signal_ehpairs_vs_pt_2d",PLOT_DIR, interactivePlots)
 
 # --- Plot 5: charge separation for low and high pt ---
 def plotPtLowHigh(truthbib, truthsig, mask_bib,mask_sig,PLOT_DIR="./plots",interactivePlots=False):
@@ -747,17 +758,17 @@ def plotPtLowHigh(truthbib, truthsig, mask_bib,mask_sig,PLOT_DIR="./plots",inter
     # Example: plot pt distributions for low/high pt, positive/negative charge
     fig, ax = plt.subplots(2,2,figsize=(12,5))
     plt.subplot(222)
-    plotManyHisto([truthSigLowPos['pt'],truthSigLowNeg['pt']],bins=30,title='Sig Low pt (<5 GeV)',pltStandalone=False,
-                  pltLabels=['Low pt, q>0','Low pt, q<0'],xlabel='pt (GeV)',yscale='log',)
+    plotManyHisto([truthSigLowPos['pt'],truthSigLowNeg['pt']],bins=30,title='Sig Low pt (<5 GeV/c)',pltStandalone=False,
+                  pltLabels=['Low pt, q>0','Low pt, q<0'],xlabel='pt [GeV/c]',yscale='log',)
     plt.subplot(224)
-    plotManyHisto([truthSigHighPos['pt'],truthSigHighNeg['pt']],bins=30,title='Sig High pt (>95 GeV)',pltStandalone=False,
-                  pltLabels=['High pt, q>0','High pt, q<0'],xlabel='pt (GeV)')   
+    plotManyHisto([truthSigHighPos['pt'],truthSigHighNeg['pt']],bins=30,title='Sig High pt (>95 GeV/c)',pltStandalone=False,
+                  pltLabels=['High pt, q>0','High pt, q<0'],xlabel='pt [GeV/c]')   
     plt.subplot(221)
-    plotManyHisto([truthBibLowPos['pt'],truthBibLowNeg['pt']],bins=30,title='Bib Low pt (<5 GeV)',pltStandalone=False,
-                  pltLabels=['Low pt, q>0','Low pt, q<0'],xlabel='pt (GeV)',yscale='log',)
+    plotManyHisto([truthBibLowPos['pt'],truthBibLowNeg['pt']],bins=30,title='Bib Low pt (<5 GeV/c)',pltStandalone=False,
+                  pltLabels=['Low pt, q>0','Low pt, q<0'],xlabel='pt [GeV/c]',yscale='log',)
     plt.subplot(223)
-    plotManyHisto([truthBibHighPos['pt'],truthBibHighNeg['pt']],bins=30,title='Bib High pt (>95 GeV)',pltStandalone=False,
-                  pltLabels=['High pt, q>0','High pt, q<0'],xlabel='pt (GeV)')
+    plotManyHisto([truthBibHighPos['pt'],truthBibHighNeg['pt']],bins=30,title='Bib High pt (>95 GeV/c)',pltStandalone=False,
+                  pltLabels=['High pt, q>0','High pt, q<0'],xlabel='pt [GeV/c]')
 
 
     closePlot(PLOT_DIR, interactivePlots, "ELIZAHATESTHIS_bib_signal_pt_charge_separation.png")
@@ -779,7 +790,7 @@ def plotRadius(truthbib, truthsig,PLOT_DIR="./plots",interactivePlots=False):
     plt.legend()
     plt.title("Radius of track curvature vs. pt")
     plt.xlabel("Radius [mm]")
-    plt.ylabel("pT [GeV]")
+    plt.ylabel("pT [GeV/c]")
     closePlot(PLOT_DIR, interactivePlots,  "radiusPlot.png")
 
 #Some of these may be redundant
@@ -1004,10 +1015,10 @@ def plotTrackPPt(tracksBib, tracksSig,binsBib=30,binsSig=30,yscale='log',PLOT_DI
     fig, ax=plt.subplots(ncols=2, nrows=1, figsize=(10,5))
     plt.subplot(121)
     plotManyHisto([tracksBib["p"],tracksBib["pt"]],binsBib,title="BIB tracklists, p and pT",yscale=yscale,
-                  pltLabels=["p","pT"],xlabel="Momentum (GeV)",pltStandalone=False,alphas=[1,0.5])
+                  pltLabels=["p","pT"],xlabel="Momentum [GeV/c]",pltStandalone=False,alphas=[1,0.5])
     plt.subplot(122)
     plotManyHisto([tracksSig["p"],tracksSig["pt"]],binsSig,title="Sig tracklists, p and pT",yscale=yscale,
-                  pltLabels=["p","pT"],xlabel="Momentum (GeV)",pltStandalone=False,alphas=[1,0.5],)
+                  pltLabels=["p","pT"],xlabel="Momentum [GeV/c]",pltStandalone=False,alphas=[1,0.5],)
 
     closePlot(PLOT_DIR, interactivePlots,  f"TrackPPt.png")
 
@@ -1016,7 +1027,7 @@ def plotPtTrackAndParquet(tracksBib, tracksSig,truthBib, truthSig,PLOT_DIR="./pl
     key = "pt"
     binsBib = 30
     binsSig = 30
-    plotKeyTrackParquet(tracksBib, tracksSig,truthBib, truthSig,key,binsBib=binsBib, binsSig=binsSig,PLOT_DIR=PLOT_DIR,interactivePlots=interactivePlots,xlabel="pT (GeV)")
+    plotKeyTrackParquet(tracksBib, tracksSig,truthBib, truthSig,key,binsBib=binsBib, binsSig=binsSig,PLOT_DIR=PLOT_DIR,interactivePlots=interactivePlots,xlabel="pT [GeV/c]")
 
 def plotPCalcTrackComparison(tracksDF,bibSigLabel="BIBORSIG",PLOT_DIR="./plots",interactivePlots=False):
     # z = 1./np.sqrt((1.+tracksDF["cotb"]*tracksDF["cotb"]+tracksDF["cota"]*tracksDF["cota"]))
@@ -1027,10 +1038,10 @@ def plotPCalcTrackComparison(tracksDF,bibSigLabel="BIBORSIG",PLOT_DIR="./plots",
     z = tracksDF['z']
 
     p = tracksDF["pt"] / np.sqrt((z**2 +y**2)/(x**2 +y**2 +z**2 ))
-
+    assert len(p)==len(tracksDF["p"])
     fig, ax=plt.subplots(ncols=2, nrows=1, figsize=(10,5))
     plt.subplot(121)    
-    plotManyHisto([p,tracksDF["p"]],pltStandalone=False,title=f"{bibSigLabel}",xlabel="Momentum (GeV)",yscale='log',alphas=[1,0.5],
+    plotManyHisto([p,tracksDF["p"]],pltStandalone=False,title=f"{bibSigLabel}",xlabel="Momentum [GeV/c]",yscale='log',alphas=[1,0.5],
                   pltLabels=["p recalculated using \n cota, cotb, pt, in the tracklists","p directly as saved in tracklists"],)
 
     plt.subplot(122)
@@ -1038,7 +1049,7 @@ def plotPCalcTrackComparison(tracksDF,bibSigLabel="BIBORSIG",PLOT_DIR="./plots",
     plt.title(f"Difference between p saved in {bibSigLabel} tracklists and \n p recalculated from cota, cotb, pt saved in tracklists")
     plt.yscale('log')
     plt.ylabel("N tracks")
-    plt.xlabel('Momentum - momentum = "0" (GeV)')
+    plt.xlabel('Momentum - momentum = "0" [GeV/c]')
 
     closePlot(PLOT_DIR, interactivePlots,  f"TrackPCalcComparison{bibSigLabel}.png")
 
@@ -1092,11 +1103,11 @@ def plotNxyzTrackParquet(tracksBib, tracksSig,truthBib, truthSig,PLOT_DIR="./plo
     fig, ax=plt.subplots(ncols=2, nrows=3, figsize=(10,13))
 
     key = "n_x"
-    plotKeyTrackParquet(tracksBib, tracksSig,truthBib, truthSig,key,binsBib=binsBib, binsSig=binsSig,recalcStr=recalcStr,PLOT_DIR=PLOT_DIR,interactivePlots=interactivePlots,isSubplot=True,subplots=[321,322],xlabel = "momentum*scalePion (GeV*scalePion)")
+    plotKeyTrackParquet(tracksBib, tracksSig,truthBib, truthSig,key,binsBib=binsBib, binsSig=binsSig,recalcStr=recalcStr,PLOT_DIR=PLOT_DIR,interactivePlots=interactivePlots,isSubplot=True,subplots=[321,322],xlabel = "momentum*scalePion (GeV/c*scalePion)")
     key = "n_y"
-    plotKeyTrackParquet(tracksBib, tracksSig,truthBib, truthSig,key,binsBib=binsBib, binsSig=binsSig,recalcStr=recalcStr,PLOT_DIR=PLOT_DIR,interactivePlots=interactivePlots,isSubplot=True,subplots=[323,324],xlabel = "momentum*scalePion (GeV*scalePion)")
+    plotKeyTrackParquet(tracksBib, tracksSig,truthBib, truthSig,key,binsBib=binsBib, binsSig=binsSig,recalcStr=recalcStr,PLOT_DIR=PLOT_DIR,interactivePlots=interactivePlots,isSubplot=True,subplots=[323,324],xlabel = "momentum*scalePion (GeV/c*scalePion)")
     key = "n_z"
-    plotKeyTrackParquet(tracksBib, tracksSig,truthBib, truthSig,key,binsBib=binsBib, binsSig=binsSig,recalcStr=recalcStr,PLOT_DIR=PLOT_DIR,interactivePlots=interactivePlots,isSubplot=True,subplots=[325,326],xlabel = "momentum*scalePion (GeV*scalePion)")
+    plotKeyTrackParquet(tracksBib, tracksSig,truthBib, truthSig,key,binsBib=binsBib, binsSig=binsSig,recalcStr=recalcStr,PLOT_DIR=PLOT_DIR,interactivePlots=interactivePlots,isSubplot=True,subplots=[325,326],xlabel = "momentum*scalePion (GeV/c*scalePion)")
     closePlot(PLOT_DIR, interactivePlots, "TrackParquet_nxnynz.png")
 
 def plotKeyTrackParquet(tracksBib, tracksSig,truthBib, truthSig,key,binsBib=30, binsSig=30, recalcStr = "",