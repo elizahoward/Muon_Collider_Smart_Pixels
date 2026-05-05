e0197878f363166d4052647954327d48b4e586f0
Tsz Ngong You <youeric@uchicago.edu>
2026-05-05 12:09:12 -0500

    eric try again

---
+++ eric/model3_8bit_normalised_selected/pareto_primary/hls_outputs/hls_model_trial_110/firmware/weights/b27.h
@@ -0,0 +1,15 @@
+//Numpy array shape [1]
+//Min 0.093750000000
+//Max 0.093750000000
+//Number of zeros 0
+
+#ifndef B27_H_
+#define B27_H_
+
+#ifndef __SYNTHESIS__
+bias27_t b27[1];
+#else
+bias27_t b27[1] = {0.0937500};
+#endif
+
+#endif
---
+++ eric/model3_8bit_normalised_selected/pareto_primary/hls_outputs/hls_model_trial_110/firmware/weights/b9.h
@@ -0,0 +1,15 @@
+//Numpy array shape [8]
+//Min -0.179687500000
+//Max 0.000000000000
+//Number of zeros 2
+
+#ifndef B9_H_
+#define B9_H_
+
+#ifndef __SYNTHESIS__
+bias9_t b9[8];
+#else
+bias9_t b9[8] = {0.0000000, -0.0078125, -0.0078125, -0.0078125, -0.0546875, -0.1796875, 0.0000000, -0.0156250};
+#endif
+
+#endif
