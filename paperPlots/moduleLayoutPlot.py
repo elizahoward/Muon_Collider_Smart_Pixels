857bfb9a99b56a4482caab8844960b82666d1c3f
dsabadjiev <dabadjiev@uchicago.edu>
2026-07-30 05:08:38 -0500

    Made module layout plots use seaborn poster stylesheet

--- paperPlots/moduleLayoutPlot.py
+++ paperPlots/moduleLayoutPlot.py
@@ -7,16 +7,20 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 import sys
+import matplotlib
 
 sys.path.append("../MuC_Smartpix_Data_Production/Tracklist_Production")
 sys.path.append("../daniel/validationPlots")
 
 
 plt.style.use('seaborn-v0_8-colorblind')
+plt.style.use('seaborn-v0_8-poster')
+
+matplotlib.rcParams["figure.dpi"] = 300
 
 # from coordinateValidation_Utils import * #instead of importing, copied function over
 # def plotHits(sig,uniqueGammas,pltStandalone=True,pltShow=True,alpha = 1):
-def plotHitsXY(sig,pltStandalone=True,pltShow=True,alpha = 1,figsize=(4,3.8)):
+def plotHitsXY(sig,pltStandalone=True,pltShow=True,alpha = 1,figsize=(4,3.8)): #figsize defaults for when not using poster stylesheet
     if pltStandalone:
         fig,ax=plt.subplots(figsize=figsize)
     else:
@@ -29,7 +33,7 @@ def plotHitsXY(sig,pltStandalone=True,pltShow=True,alpha = 1,figsize=(4,3.8)):
     # uniqueGammas = np.unique(sig['gamma'])
     # colors = sig['gamma'].apply(lambda gamma: colorsList[list(uniqueGammas).index(gamma)])
     colors = sig['moduleID'].apply(lambda ID: colorsList[list(np.arange(1,17,1)).index(ID)])
-    ax.scatter(sig['hit_x'],sig["hit_y"],s=1, c=colors, label="Signal hits",alpha = alpha)
+    ax.scatter(sig['hit_x'],sig["hit_y"],s=10, c=colors, label="Signal hits",alpha = alpha)
     plt.gca().set_aspect('equal')
     circle=plt.Circle((0,0), 30, fill=0, color='k', label="Barrel")
     ax.add_patch(circle)
@@ -42,7 +46,7 @@ def plotHitsXY(sig,pltStandalone=True,pltShow=True,alpha = 1,figsize=(4,3.8)):
     if pltShow:
         plt.show()
 
-def plotRZPlane(sig,figsize=(4,1.5)):
+def plotRZPlane(sig,figsize=(4,1.5)):#figsize defaults for when not using poster stylesheet
     print(sig)
     print(sig.keys())
     plt.figure(figsize=figsize)
@@ -55,7 +59,7 @@ def plotRZPlane(sig,figsize=(4,1.5)):
 
     sig['hit_r'] = np.sqrt( sig["hit_y"]**2 + sig["hit_x"]**2)
     # plt.subplot(211)
-    plt.scatter(sig['zglobal'],sig["hit_r"],marker="s",s=2,c=colors, label="Signal hits")
+    plt.scatter(sig['zglobal'],sig["hit_r"],marker="s",s=10,c=colors, label="Signal hits")
     # plt.ylim([0,60])
     # plt.subplot(212)
     # plt.scatter(sig['hit_z'],sig["hit_r"],s=1,c=colors, label="Signal hits") #to confirm that hit_z is the same as z-global
@@ -71,11 +75,11 @@ def main():
     sig = pd.read_csv('/local/d1/smartpixML/2026Datasets/Data_Files/Data_Set_flp_0/Track_Lists/signal_tracks_extra_info_0.txt', sep=' ')
     print(len(sig))
     plt.close()
-    plotHitsXY(sig)
+    plotHitsXY(sig,figsize=(6,5.5))
     plt.title("")
     plt.savefig("./ModuleLayoutXY.png")
     plt.close()
-    plotRZPlane(sig)
+    plotRZPlane(sig,figsize=((5,2.4)))
 
 if __name__=="__main__":
     main()
